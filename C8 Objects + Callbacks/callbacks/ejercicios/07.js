150ac90ea2d4a309ca7c4bb7e8e89d55a86559e3
dssuarezca <dssuarez02@misena.edu.co>
2024-03-17 21:05:29 -0500

    ejercicios callbakc

--- C8 Objects + Callbacks/callbacks/ejercicios/07.js
+++ C8 Objects + Callbacks/callbacks/ejercicios/07.js
@@ -2,6 +2,19 @@ function filter(arrayOfStrings) {
    // Debes identificar todos los elementos el arreglo que comiencen con la letra "a".
    // Luego retorna un nuevo arreglo con estos elementos.
    // Tu código:
+   nuevoarreglo=[]
+   caracter=[]
+   j=0
+   for(var i=0;i<arrayOfStrings.length;i++){
+      caracter=arrayOfStrings[i].split("")
+      console.log(caracter)
+      if (caracter[0]==="a"){
+         console.log(nuevoarreglo[j]=arrayOfStrings[i])
+         console.log(nuevoarreglo[j])
+         j++
+      }
+     }
+     return nuevoarreglo
 }
-
+filter(["hola","caja","avion"])
 module.exports = filter;
