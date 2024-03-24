2b0d8463eab03a0ff354f321aba8d4d5fba7d2f6
dssuarezca <dssuarez02@misena.edu.co>
2024-03-24 18:53:41 -0500

    dfdfdfdf

--- C8 Objects + Callbacks/callbacks/ejercicios/07.js
+++ C8 Objects + Callbacks/callbacks/ejercicios/07.js
@@ -2,19 +2,18 @@ function filter(arrayOfStrings) {
    // Debes identificar todos los elementos el arreglo que comiencen con la letra "a".
    // Luego retorna un nuevo arreglo con estos elementos.
    // Tu código:
-   nuevoarreglo=[]
-   caracter=[]
+   nuevoarreglo = []
+   caracter = []
    j=0
-   for(var i=0;i<arrayOfStrings.length;i++){
-      caracter=arrayOfStrings[i].split("")
+   for (var i = 0; i < arrayOfStrings.length; i++) {
+      caracter = arrayOfStrings[i].split("")
       console.log(caracter)
-      if (caracter[0]==="a"){
-         console.log(nuevoarreglo[j]=arrayOfStrings[i])
+      if (caracter[0] === "a") {
+         console.log(nuevoarreglo[j] = arrayOfStrings[i])
          console.log(nuevoarreglo[j])
-         j++
       }
-     }
-     return nuevoarreglo
+   }
+   return nuevoarreglo
 }
-filter(["hola","caja","avion"])
+filter(["hola", "caja", "avion","dfdgfg", "avgfyrgf"])
 module.exports = filter;
--- C8 Objects + Callbacks/objects/ejercicios/pruebas.js
+++ C8 Objects + Callbacks/objects/ejercicios/pruebas.js
@@ -1,13 +1,2 @@
-objeto={
-    precio:5,
-    preciodescuento:0.5
-};
-Object.defineProperties(objeto,{
-    DORMIR:{
-        value:function (){
-            preciofinal=this.precio-(this.precio*this.preciodescuento)
-            return preciofinal
-        }
-    }
-});
-console.log(objeto.DORMIR())
+var m=2+3*4/2
+console.log(m)
